b3c51092f1ab0f64478df7360e9d43fc24488fbb
ropuk <pukpen@gmail.com>
2023-06-17 17:33:46 -0300

    adiciona AllArgsConstructor

--- src/main/java/com/arantes/cleanarch/dataprovider/repository/entity/AddressEntity.java
+++ src/main/java/com/arantes/cleanarch/dataprovider/repository/entity/AddressEntity.java
@@ -1,8 +1,10 @@
 package com.arantes.cleanarch.dataprovider.repository.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class AddressEntity {
 
     private String street;
--- src/main/java/com/arantes/cleanarch/dataprovider/repository/entity/CustomerEntity.java
+++ src/main/java/com/arantes/cleanarch/dataprovider/repository/entity/CustomerEntity.java
@@ -1,11 +1,13 @@
 package com.arantes.cleanarch.dataprovider.repository.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.apache.logging.log4j.message.AsynchronouslyFormattable;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@AllArgsConstructor
 @Document(collection = "customers")
 public class CustomerEntity {
 
