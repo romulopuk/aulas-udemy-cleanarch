eaa7d101b271f9d031c2533926fbcd8e29e18d99
ropuk <pukpen@gmail.com>
2023-06-17 14:52:35 -0300

    altera Address para receber Response

--- src/main/java/com/arantes/cleanarch/core/domain/Address.java
+++ src/main/java/com/arantes/cleanarch/core/domain/Address.java
@@ -1,8 +1,14 @@
 package com.arantes.cleanarch.core.domain;
 
+import com.arantes.cleanarch.dataprovider.client.response.AddressResponse;
+
 public record Address(
         String street,
         String city,
         String state
 ) {
+
+    public Address(AddressResponse response) {
+        this(response.street(), response.city(), response.state());
+    }
 }
