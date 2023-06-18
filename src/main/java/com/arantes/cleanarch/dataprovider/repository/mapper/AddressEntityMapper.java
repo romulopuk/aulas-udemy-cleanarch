c5c86e37d2377821abad6ac385646fc596c6ff01
ropuk <pukpen@gmail.com>
2023-06-18 16:22:26 -0300

    altera mapper utilizando mapstruct

--- src/main/java/com/arantes/cleanarch/dataprovider/repository/mapper/AddressEntityMapper.java
+++ src/main/java/com/arantes/cleanarch/dataprovider/repository/mapper/AddressEntityMapper.java
@@ -2,15 +2,12 @@ package com.arantes.cleanarch.dataprovider.repository.mapper;
 
 import com.arantes.cleanarch.core.domain.Address;
 import com.arantes.cleanarch.dataprovider.repository.entity.AddressEntity;
-import org.springframework.stereotype.Service;
+import org.mapstruct.Mapper;
 
-import java.util.function.Function;
+@Mapper(componentModel = "spring")
+public interface AddressEntityMapper {
 
-@Service
-public class AddressEntityMapper implements Function<Address, AddressEntity> {
+    AddressEntity toEntity(Address address);
 
-    @Override
-    public AddressEntity apply(Address address) {
-        return new AddressEntity(address.street(), address.city(), address.state());
-    }
+    Address toDomain(AddressEntity entity);
 }
--- src/main/java/com/arantes/cleanarch/dataprovider/repository/mapper/CustomerEntityMapper.java
+++ src/main/java/com/arantes/cleanarch/dataprovider/repository/mapper/CustomerEntityMapper.java
@@ -2,25 +2,12 @@ package com.arantes.cleanarch.dataprovider.repository.mapper;
 
 import com.arantes.cleanarch.core.domain.Customer;
 import com.arantes.cleanarch.dataprovider.repository.entity.CustomerEntity;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-import java.util.function.Function;
+@Mapper(componentModel = "spring")
+public interface CustomerEntityMapper {
 
-@Component
-@RequiredArgsConstructor
-public class CustomerEntityMapper implements Function<Customer, CustomerEntity> {
+    CustomerEntity toEntity(Customer customer);
 
-    private final AddressEntityMapper mapper;
-
-    @Override
-    public CustomerEntity apply(Customer customer) {
-        return new CustomerEntity(
-                customer.id(),
-                customer.name(),
-                mapper.apply(customer.address()),
-                customer.cpf(),
-                customer.isValidCpf()
-        );
-    }
+    Customer toDomain(CustomerEntity entity);
 }
