59ba85db18fc7ac8e34d02e0e0c2976944205e8c
ropuk <pukpen@gmail.com>
2023-06-18 16:26:03 -0300

    adiciona metodo getbyid no controller

--- src/main/java/com/arantes/cleanarch/entrypoint/controller/CustomerController.java
+++ src/main/java/com/arantes/cleanarch/entrypoint/controller/CustomerController.java
@@ -1,10 +1,14 @@
 package com.arantes.cleanarch.entrypoint.controller;
 
 import com.arantes.cleanarch.core.domain.Customer;
+import com.arantes.cleanarch.core.usecase.FindCustomerByIdUseCase;
 import com.arantes.cleanarch.core.usecase.InsertCustomerUseCase;
 import com.arantes.cleanarch.entrypoint.controller.request.CustomerRequest;
+import com.arantes.cleanarch.entrypoint.controller.response.CustomerResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +21,18 @@ import javax.validation.Valid;
 @RequestMapping("api/v1/customers")
 public class CustomerController {
 
-    private final InsertCustomerUseCase useCase;
+    private final InsertCustomerUseCase insertUseCase;
+    private final FindCustomerByIdUseCase findByIdUseCase;
 
     @PostMapping
     public ResponseEntity<Void> insert(@Valid @RequestBody CustomerRequest request) {
-        useCase.insert(new Customer(request), request.zipCode());
+        insertUseCase.insert(new Customer(request), request.zipCode());
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerResponse> findById(@PathVariable final String id) {
+        var result = new CustomerResponse(findByIdUseCase.find(id));
+        return ResponseEntity.ok().body(result);
     }
 }
